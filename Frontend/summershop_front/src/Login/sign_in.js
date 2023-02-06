30466ccc162136166a3f9939184c98fa7a5d8fa7
Narayan Chuwan <chuwan2001@gmail.com>
2023-02-05 21:07:10 -0600

    first_commit

---
+++ Frontend/summershop_front/src/Login/sign_in.js
@@ -0,0 +1,33 @@
+import "./login_css/sign_in.css";
+
+function sign_in() {
+    return (
+      <div id="text_form">
+        <div id="main_flex">
+            <div className="label_input">
+                <label>Username: </label>
+                <input type="text" placeholder="Username" required></input>
+            </div>
+            <div className="label_input">
+                <label>Password: </label>
+                <input type="password" placeholder="Password" required></input>
+            </div>
+            <div id="sign_in">
+                <div id="sign_button">
+                    <button>Sign In</button>
+                </div>
+                <a id="forgot">Forgot Password</a>
+                <a id="create">Create Account</a>
+
+            </div>
+        </div>
+       
+       
+      </div>
+        
+
+    );
+  }
+  
+  export default sign_in;
+  
